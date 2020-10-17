0f27da76cd10730e7465ca813f017822c3fc7b29
Jonatan Kłosko <jonatanklosko@gmail.com>
2020-10-17 12:18:26 +0200

    Fix scrmable select.

--- src/components/ScrambleSetSelect.js
+++ src/components/ScrambleSetSelect.js
@@ -2,14 +2,20 @@ import React from 'react';
 import { Select, MenuItem } from '@material-ui/core';
 
 function ScrambleSetSelect({ scrambleSets, value, onChange }) {
+  function handleSelectChange(event) {
+    const id = parseInt(event.target.value, 10);
+    const scrambleSet = scrambleSets.find(scrambleSet => scrambleSet.id === id);
+    onChange(scrambleSet);
+  }
+
   return (
     <Select
       variant="outlined"
-      value={value}
-      onChange={(event) => onChange(event.target.value)}
+      value={value.id}
+      onChange={handleSelectChange}
     >
       {scrambleSets.map((scrambleSet) => (
-        <MenuItem key={scrambleSet.id} value={scrambleSet}>
+        <MenuItem key={scrambleSet.id} value={scrambleSet.id}>
           {scrambleSet.title}
         </MenuItem>
       ))}
